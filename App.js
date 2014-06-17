754a2f64b6f4b394183b54ed4235b2f22e31225d
loaner <loaner@mdoerfler-mba.f4tech.com>
2014-06-17 10:06:42 -0600

    Completed gathering hierarchy of data

--- App.js
+++ App.js
@@ -2,75 +2,81 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     items: [
-      {
-        xtype: 'rallysolrartifactchooserdialog',
-        id: 'portfolioItemPicker',
-        artifactTypes: ['portfolioitem/initiative'],
-        autoShow: true,
-        height: 250,
-        title: 'Choose an Initiative',
-        listeners: {
-            artifactChosen: function(picker, selectedRecord) {
-                App._getArtifacts(selectedRecord);
-            },
-            scope: this
+        {
+            xtype: 'rallysolrartifactchooserdialog',
+            id: 'portfolioItemPicker',
+            artifactTypes: ['portfolioitem/initiative'],
+            autoShow: true,
+            height: 250,
+            title: 'Choose an Initiative',
+            listeners: {
+                artifactChosen: function(picker, selectedRecord) {
+                    App._getArtifacts(selectedRecord);
+                },
+                scope: this
+            }
         }
-      }
     ],
+
     launch: function() {
-        //Write app code here
         App = this;
     },
 
-    _passFn: function(result) {
-        console.log('success', result);
-    },
-
-    _failFn: function(result) {
-        console.log('fail', result);
-    },
-
+    /*
+     * Get all artifacts that fall within the givin scope
+     * and organize them into a logically grouped mapping
+     */
     _getArtifacts: function(scope) {
         var hierarchy = scope.get('ObjectID');
         console.log("Querying lbAPI for artifacts under " + hierarchy);
 
-        App.organizedData = {
+        aggregateData = {
             initiative: scope.data,
-            features: [],
-            stories: [],
-            defects: [],
-            tasks: [],
-
+            features: {},
+            storiesAndDefects: {},
+            parentless: []
         };
+
+        var tasks = [];
 
         Ext.getBody().mask('Loading');
         Ext.create('Rally.data.lookback.SnapshotStore', {
             listeners: {
                 load: function(store, data, success) {
-                    // TODO optimize
-                    Ext.getBody().unmask();
+                    // Aggregate data
                     Ext.Array.each(data, function(artifact) {
                         var types = artifact.raw._TypeHierarchy;
-                        console.log(types);
                         switch (types[types.length - 1]) {
                             case "Defect":
-                                Ext.Array.push(App.organizedData.defects, artifact.data);
+                                aggregateData.storiesAndDefects[artifact.data.ObjectID] = {
+                                    artifact: artifact.raw,
+                                    children: []
+                                };
                                 break;
                             case "HierarchicalRequirement":
-                                Ext.Array.push(App.organizedData.stories, artifact.data);
+                                aggregateData.storiesAndDefects[artifact.data.ObjectID] = {
+                                    artifact: artifact.raw,
+                                    children: []
+                                };
                                 break;
-                            case "PortfolioItem/Feature": // TODO PI/Feature or Feature
-                                Ext.Array.push(App.organizedData.features, artifact.data);
+                            case "PortfolioItem/Feature":
+                                aggregateData.features[artifact.data.ObjectID] = {
+                                    feature: artifact.raw,
+                                    children: []
+                                };
                                 break;
                             case "Task":
-                                Ext.Array.push(App.organizedData.tasks, artifact.data);
+                                Ext.Array.push(tasks, artifact.raw);
                                 break;
                             default: // ignore
                         }
                     });
-                    // App._getStories(hierarchy);
-                    console.log(App.organizedData);
 
+                    var organizedData = App._organizeData(aggregateData, tasks);
+                    console.log(organizedData);
+                    Ext.getBody().unmask();
+
+                    // From here on out we want an angular/melon/js app
                 }
             },
             fetch: true,
@@ -80,41 +86,69 @@ Ext.define('CustomApp', {
                 "__At" : "current"
             }
         }).load();
-
-        /*
-
-        //process data
-        console.log(store);
-        console.log(success);
-        console.log('data returned', data);
-
-        Ext.getBody().unmask();
-        if (success) {
-          console.log(data);
-          if (data.length === 0) {
-              Ext.Msg.alert('Nothing here...', 'No data returned');
-          }
-          App._processData(scope, data);
-        } else {
-          Ext.Msg.alert('Error', 'Failed to get associated artifacts.  Please try again');
-        }
-
-        */
     },
 
-    _processData: function(scope, data) {
-        console.log('processing', scope, data);
-        var organizedData = {
-            initiative: {
+    /*
+     * Create a hierarchy of data
+     * Map Tasks to their Story/Defect
+     * Map Stories/Defects to a feature
+     * Map the initiative to its features
+     */
+    _organizeData: function(aggregateData, tasks) {
 
-                features: []
-            }
+        // organizedData
+        //   |
+        //   +-->Initiative
+        //   +-->Features
+        //       |
+        //       +-->Stories/Defects
+        //           |
+        //           +-->Tasks
+        var organizedData = {
+            initiative: aggregateData.initiative
         };
 
-        Ext.Array.each(data, function(one) {
-            // if it is a feature add it
+        // Add tasks as children of their associated story/defect
+        Ext.Array.each(tasks, function(task) {
+            var parent = task.WorkProduct;
 
-            console.log(one.get('FormattedID'));
+            if (aggregateData.storiesAndDefects[parent]) {
+                // add it to the list of children
+                Ext.Array.push(aggregateData.storiesAndDefects[parent].children, task);
+            } else {
+                // not parented to a story/defect
+                console.log('not parented to a story or defect', task);
+            }
         });
+
+        // Add stories and tasks as children of their feature
+        Ext.Object.each(aggregateData.storiesAndDefects, function(oid, object) {
+            var parent;
+            var types = object.artifact._TypeHierarchy;
+
+            if (types[types.length - 1] === "HierarchicalRequirement") {
+                // stories have an associated PortfolioItem/Feature
+                parent = object.artifact.PortfolioItem;
+            } else {
+                // defects associate with a story or defect that will parent to a Feature
+                var requirement = aggregateData.storiesAndDefects[object.artifact.Requirement];
+                if (requirement) {
+                    parent = requirement.artifact.PortfolioItem;
+                } else {
+                    console.log('not found', object);
+                }
+            }
+
+            if (aggregateData.features[parent]) {
+                // add it to the list of children
+                Ext.Array.push(aggregateData.features[parent].children, object);
+            } else {
+                // not parented to a feature
+                console.log('not parented to a feature');
+            }
+        });
+
+        organizedData.features = aggregateData.features;
+        return organizedData;
     }
 });
